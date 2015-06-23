81a5dbd611f1f22c02de09de3ac7a77a21321a1f
Andy Armstrong <andy@hexten.net>
2015-06-23 22:49:52 +0100

    Another WIP snapshot.

--- index.js
+++ index.js
@@ -5,24 +5,14 @@ var url = require("url");
 var _ = require("underscore");
 var Promise = require("bluebird");
 
-function onError(thing, cb) {
-  return function(err, rows, fields) {
-    if (err) {
-      var msg = thing + " failed: " + err.code + "(" + err.errno + ")";
-      throw new Error(msg);
-    }
-    if (cb) {
-      cb(rows, fields);
-    }
-  };
-}
-
 function Driver(config) {
   this.config = _.extend({
     prefix: "_squnk_meta_"
   }, config);
 
   this.connection = null;
+  this.tableInfoCache = {};
+  this.metaTable = null;
 }
 
 Driver.prototype.parseConnectionURI = function(uri) {
@@ -152,10 +142,11 @@ Driver.prototype.query = function(sql) {
         if (err) {
           reject(err);
         }
-        console.log("result: " + JSON.stringify({
-          rows: rows,
-          fields: fields
-        }, null, 2));
+        console.log("query: " + sql);
+        //        console.log("result: " + JSON.stringify({
+        //          rows: rows,
+        //          fields: fields
+        //        }, null, 2));
         resolve([rows, fields]);
       });
     })
@@ -163,7 +154,6 @@ Driver.prototype.query = function(sql) {
 };
 
 Driver.prototype.runScript = function(script) {
-  var conn = this.getConnection();
   var p = Promise.bind(this);
 
   this.parseScript(script)
@@ -191,41 +181,149 @@ Driver.prototype.runScript = function(script) {
 
 // Create our tables (private)
 
-Driver.prototype.makeMetaTable = function(table) {
+Driver.prototype.format = function() {
   var conn = this.getConnection();
-  var create = conn.format("CREATE TABLE ?? (" +
-    "  name VARCHAR(80) NOT NULL" + ")", [table])
-  return this.query(create)
-    .spread(function(rows, fields) {
-      console.log("result: " + JSON.stringify({
-        rows: rows,
-        fields: fields
-      }, null, 2));
-      return table;
+  var args = _.toArray(arguments);
+  var sql = args.shift();
+  var flatArgs = args.map(function(arg) {
+    return _.flatten(arg, true);
+  });
+  flatArgs.unshift(sql);
+  return conn.format.apply(conn, flatArgs);
+};
+
+Driver.prototype.getMetaTableName = function() {
+  return this.config.prefix + "deltas";
+};
+
+Driver.prototype.makeMetaTable = function() {
+  var conn = this.getConnection();
+  var table = this.getMetaTableName();
+
+  var script = [
+      "-- * Creating ??",
+      "DROP TABLE IF EXISTS ??;", [
+        "CREATE TABLE ?? (",
+        "  `name` varchar(80) NOT NULL COMMENT 'The name of the delta',",
+        "  `sequence` int(10) unsigned NOT NULL COMMENT 'The sequence number of the delta',",
+        "  `state` varchar(20) NOT NULL DEFAULT 'pending' COMMENT 'The state of the delta',",
+        "  `delta` mediumtext COMMENT 'JSONball of the scripts',",
+        "  `meta` mediumtext COMMENT 'JSON metadata bundle',",
+        "  PRIMARY KEY (`name`),",
+        "  UNIQUE KEY `sequence` (`sequence`),",
+        "  KEY `state` (`state`)",
+        ");"
+      ].join(" ")
+    ].map(function(ln) {
+      return this.format(ln, [table]);
+    }, this)
+    .join("\n");
+
+  return this.runScript(script);
+
+};
+
+Driver.prototype.getTableInfo = function(table) {
+  var cache = this.tableInfoCache;
+  if (cache.hasOwnProperty(table)) {
+    return cache[table];
+  }
+  cache[table] = this.query(this.format("DESCRIBE ??", [table]));
+  return cache[table];
+};
+
+Driver.prototype.getTableColumns = function(table) {
+  return this.getTableInfo(table)
+    .spread(function(rows) {
+      var pk = [];
+      var cols = [];
+      var all = [];
+      rows.forEach(function(row) {
+        all.push(row["Field"]);
+        if (row["Key"] === "PRI") {
+          pk.push(row["Field"]);
+        } else {
+          cols.push(row["Field"]);
+        }
+      });
+      return {
+        pk: pk,
+        cols: cols,
+        all: all
+      };
     });
 };
 
 Driver.prototype.getMetaTable = function() {
-  var conn = this.getConnection();
-  var table = this.config.prefix + "deltas";
+  var table = this.getMetaTableName();
 
-  return this.query(conn.format("SHOW TABLES LIKE ?", [table]))
-    .spread(function(rows, fields) {
-      console.log("result: " + JSON.stringify({
-        rows: rows,
-        fields: fields
-      }, null, 2));
-      if (rows.length === 0) {
-        return this.makeMetaTable(table);
-      }
-      return table;
-    });
+  if (this.metaTable === null) {
+    this.metaTable = this.query(this.format("SHOW TABLES LIKE ?", [table]))
+      .spread(function(rows) {
+        if (rows.length === 0) {
+          return this.makeMetaTable()
+            .return(table);
+        }
+        return table;
+      })
+      .then(function(tableName) {
+        return [tableName, this.getTableColumns(tableName)];
+      });
+  }
+
+  return this.metaTable;
 };
+
+function placeholder(str, rep) {
+  var ph = [];
+  for (var i = 0; i < rep; i++) {
+    ph.push(str);
+  }
+  return ph.join(", ");
+}
+
+function indexedValues(obj, idx) {
+  var donor = _.clone(obj);
+  var missing = [];
+  var vals = idx.map(function(key) {
+    if (!donor.hasOwnProperty(key)) {
+      missing.push(key);
+      return null;
+    }
+    var v = donor[key];
+    delete donor[key];
+    return v;
+  }, this);
+  var extra = _.keys(donor);
+  if (missing.length || extra.length) {
+    var and = [];
+    if (missing.length) {
+      and.push("is missing field(s): " + missing.join(", "));
+    }
+    if (extra.length) {
+      and.push("has these unknown field(s): " + extra.join(", "));
+    }
+    throw new Error("Data" + and.join(" and "))
+  }
+  return vals;
+}
 
 // Functions to manipulate deltas
 
-Driver.prototype.saveDelta = function(name, delta) {};
-Driver.prototype.loadDelta = function(name) {};
+Driver.prototype.saveDelta = function(delta) {
+  return this.getMetaTable()
+    .spread(function(table, columns) {
+      var cols = columns.all;
+      var bind = [table, cols, indexedValues(delta, cols)];
+      var sql = this.format("REPLACE INTO ?? (" + placeholder("??", cols.length) +
+        ") VALUES (" + placeholder("?", cols.length) + ")", bind);
+      return this.query(sql);
+    });
+};
+
+Driver.prototype.loadDelta = function(name) {
+
+};
 Driver.prototype.loadDeltas = function(name) {};
 
 Driver.prototype.setDeltaState = function(name, state) {
